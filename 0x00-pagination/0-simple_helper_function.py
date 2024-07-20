13dad64f2dd1d6423cd3f68f9ceb3907692190e4
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-20 18:41:50 +0100

    Pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" A fucntion that takes two arguents of integer and  returns a tuple """
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Start and end index that matches with range"""
+    return ((page - 1) * page_size, page_size * page) 
