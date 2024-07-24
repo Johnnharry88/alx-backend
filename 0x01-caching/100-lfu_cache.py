8106e4ec79bfdc4104411003892d26473b1c4d9f
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-24 12:37:51 +0100

    Cachig

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -3,7 +3,6 @@
 least frequently used cache"""
 from base_caching import BaseCaching
 from collections import OrderedDict
-import operator
 
 
 class LFUCache(BaseCaching):
@@ -12,30 +11,55 @@ class LFUCache(BaseCaching):
         """Constructor"""
         super().__init__()
         self.reorder = OrderedDict()
-        self.arranged = None
+        self.arranged = []
+
+    def render_reorder(self, r_ukeyx):
+        """Rearranges the items in the cache based on
+        recentely used item"""
+        mx_pos = []
+        r_ufreq = 0
+        r_upos = 0
+        i_pos = 0
+        for x,  y in enumerate(self.arranged):
+            if y[0] == r_ufreq:
+                r_ufreq = y[1] + 1
+                r_upos = x
+                break
+            elif len(mx_pos) == 0:
+                mx_pos.append(x)
+            elif y[1] < self.arranged[mx_pos[-1]][1]:
+                mx_pos.append(x)
+        mx_pos.reverse()
+        for p in mx_pos:
+            if self.arranged[p][1] > r_ufreq:
+                break
+            i_pos = p
+        self.arranged.pop(r_upos)
+        self.arranged.insert(i_pos, [r_ukeyx, r_ufreq])
 
     def put(self, key, item):
         """Places items in cache system"""
         if key is None or item is None:
             return
-        if len(self.cache_data) > self.MAX_ITEMS and key not in self.reorder:
-            self.arranged = sorted(
-                self.reorder.items(), key=operator.itemgetter(1))
-            rm = self.arranged.pop(0)[0]
-            del self.reorder[rm]
-            del self.cache_data[rm]
-            print('DISCARD: {}'.format(rm))
-
-        if key and item:
-            val = 0
-            if key in self.cache_data:
-                val = self.reorder[key]
-                del self.reorder[key]
-            self.reorder[key] = val + 1
+        if key not in self.cache_data:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                least_fkyz, _ = self.arranged[-1]
+                self.cache_data.pop(least_fkyz)
+                self.arranged.pop()
+                print('DISCARD: {}'.format(least_fkyz))
             self.cache_data[key] = item
+            i_index = len(self.arranged)
+            for x, y in enumerate(self.arranged):
+                if y[1] == 0:
+                    i_index = x
+                    break
+            self.arranged.insert(i_index, [key, 0])
+        else:
+            self.cache_data[key] = item
+            self.render_reorder(key)
 
     def get(self, key):
         """Gets data from the cache with key"""
-        if not key or key is None:
-            return
-        return self.cache_data.get(key)
+        if key is not None and key in self.cache_data:
+            self.render_reorder(key)
+        return self.cache_data.get(key, None)
