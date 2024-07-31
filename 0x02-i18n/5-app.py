216ee3b59283c304fc67fc3695f045d6562d8b29
Johnharry88 <harryjohnwhye@gmail.com>
2024-07-31 08:00:31 -0700

    i18n

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+""" Flask app that handles user"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    """Returns appropriate languages"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# app configuration for Flask
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+# User login Databas
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> str:
+    """returns the users dictionary or none if no id"""
+    id_login = request.args.get('login_as')
+    if id_login:
+        return users.get(int(id_login))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """Assigns user to a global variable g"""
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """gets the locale language specific to a user"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """Returns the index page requested"""
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port='5000', host='0.0.0.0', debug=True)
